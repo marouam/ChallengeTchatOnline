b0a72295d05ed30227bafb0eae29af06ff1fd657
marouam <39883837+marouam@users.noreply.github.com>
2019-05-21 12:42:10 +0100

    footer

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+ <!-- Javascript -->
+        <script src="public/js/jquery-1.11.1.min.js"></script>
+        <script src="public/bootstrap/js/bootstrap.min.js"></script>
+        <script src="public/js/jquery.backstretch.min.js"></script>
+        <script src="public/js/scripts.js"></script>
+		<!--[if lt IE 10]>
+            <script src="public/js/placeholder.js"></script>
+        <![endif]-->
+</html>
